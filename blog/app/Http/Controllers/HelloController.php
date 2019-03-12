277ec6f292befb0e23decc889c689d20542ff303
daisuke <dsaito85a@gmail.com>
2019-03-13 00:18:08 +0900

    パラメータ結合

--- blog/app/Http/Controllers/HelloController.php
+++ blog/app/Http/Controllers/HelloController.php
@@ -7,10 +7,17 @@ use Illuminate\Support\Facades\DB;
 
 class HelloController extends Controller
 {
-    public function index(){
-        $items = DB::select('select * from people');
-        return view('hello.index',['items' => $items]);
+    public function index(Request $request)
+    {
+        if (isset($request->id)) {
+            $param = ['id' => $request->id];
+            // $param = 2;
+            // $items = DB::select('select * from people where id = :id' , $param);
+            $items = DB::table('people')->select('*')->where('id', $param)->get();
+            // select('select * from people where id = :id' , $param);
+        } else {
+            $items = DB::select('select * from people');
+        }
+        return view('hello.index', ['items' => $items]);
     }
 }
-
-    
