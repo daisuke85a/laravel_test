f02ca2445f5634061aef06b567f056653c94bb38
daisuke <dsaito85a@gmail.com>
2019-03-12 11:26:22 +0900

    値をテンプレートに渡す

--- blog/app/Http/Controllers/HelloController.php
+++ blog/app/Http/Controllers/HelloController.php
@@ -7,7 +7,8 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
     public function index(){
-        return view('hello.index');
+        $data = ['msg'=>'これはコントローラから渡されたメッセージです。'];
+        return view('hello.index', $data);
     }
 }
 
