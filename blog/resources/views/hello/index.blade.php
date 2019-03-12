62ae74f078683b62f784ee893228bbd56ed0c4d0
daisuke <dsaito85a@gmail.com>
2019-03-12 11:56:04 +0900

    フォームを利用する

--- blog/resources/views/hello/index.blade.php
+++ blog/resources/views/hello/index.blade.php
@@ -22,5 +22,10 @@
     <p>
         {{$msg}}
     </p>
+    <form method="POST" action="/hello">
+        {{ csrf_field() }}
+        <input type="text" name="msg">
+            <input type="submit">
+    </form>
 </body>
 <html>
